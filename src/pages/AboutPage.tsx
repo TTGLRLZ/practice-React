ff51f830bb42f7c2e2d08fdfdc1a9fcef268c393
TTGLRLZ <139354959+TTGLRLZ@users.noreply.github.com>
2023-11-09 13:55:06 +0300

    finish commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id omnis expedita temporibus deserunt quo at quam eaque laboriosam est ut, minus laudantium vero soluta tempore repudiandae maiores quidem. Fuga aperiam earum placeat debitis officia. Ratione dicta aperiam hic nam incidunt obcaecati unde animi inventore quaerat quia, rerum totam quasi id!
+        </div>
+    );
+};
+
+export default AboutPage;
